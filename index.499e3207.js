2f997e808eb44810aebcd6be4787cb65413a515d
neoplanet-it <110842633+neoplanet-it@users.noreply.github.com>
2022-11-10 07:52:29 +0000

    Deploying to gh-pages from @ neoplanet-it/food-delivery-service@b3163935c8a68e4afee3a5ec4dfdc6c97e4f2b0c 🚀

---
+++ index.499e3207.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".hero__slider").slick({arrows:!0,speed:1e3,autoplay:!0,autoplayspeed:2e3,draggable:!1,swipe:!1,fade:!0})}));
+//# sourceMappingURL=index.499e3207.js.map
---
+++ index.e8e967f6.js
@@ -0,0 +1,2 @@
+var $page=$("html, body");$('a[href*="#"]').click((function(){return $page.animate({scrollTop:$($.attr(this,"href")).offset().top},1e3),!1}));
+//# sourceMappingURL=index.e8e967f6.js.map

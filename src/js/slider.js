b3163935c8a68e4afee3a5ec4dfdc6c97e4f2b0c
Taynikov Vadim <taynikov.vadim@gmail.com>
2022-11-10 08:51:40 +0100

    MAIN COMMIT
    10.11.2022

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.hero__slider').slick({
+        arrows: true,
+        speed: 1000,
+        autoplay: true,
+        autoplayspeed: 2000,
+        draggable: false,
+        swipe: false,
+        fade: true,
+    });
+});
